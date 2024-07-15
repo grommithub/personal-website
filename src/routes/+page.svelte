ea4b80e6684a7fcf929b3fc86012003d6aab7754
Dan <44238640+grommithub@users.noreply.github.com>
2024-07-15 21:10:08 +0200

    navbar

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,13 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="text-center">
+
+    <h1 class="text-3xl text-green-600">Welcome to Dan Burt's Website</h1>
+    <p>
+        Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+    </p>
+    
+    <p>
+        Press
+        <kbd class="kbd kbd-sm">F</kbd>
+        to pay respects.
+    </p>
+</div>
