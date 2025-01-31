fdb50fe14e6413a400e81e73d768f736b78c79bb
Dan Burt <gromitzy@gmail.com>
2025-01-31 01:19:34 +0100

    safari fix(?) Weird stretch may be fixed now

--- src/components/ProjectCard.svelte
+++ src/components/ProjectCard.svelte
@@ -14,7 +14,7 @@
 </script>
 
 <div
-  class="project card bg-base-300 w-full shadow-xl m-auto flex-col lg:flex-row mb-5 grid lg:grid-cols-2 justify-between overflow-hidden border-primary-content border-2"
+  class="project card bg-base-300 w-full align shadow-xl items-start m-auto flex-col lg:flex-row mb-5 grid lg:grid-cols-2 justify-between overflow-hidden border-primary-content border-2"
 in:fly={{y:1000, duration: 1000, delay:1500}}>
   <div class="w-full h-full bg-black content-center">
     <img {src} class="w-full h-fit" alt={title} 
